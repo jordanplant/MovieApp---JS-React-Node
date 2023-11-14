1a9a87b54d51556e653ce3b21e6aa3c27f6599cd
Jordan Plant <jordanplant94@gmail.com>
2023-11-14 18:52:37 +0000

    footer update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,8 +2,14 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div>
-      <p></p>
+    <div Style="display: flex; flex-direction: row; justify-content: center; align-items:center;">
+      <p Style="font-size:12px;">
+        Movie data and images courtesy of themoviedb.org{" "}
+      </p>
+      <img
+        Style="width: 100px; padding: 0px 10px;"
+        src="https://www.themoviedb.org/assets/2/v4/logos/v2/blue_short-8e7b30f73a4020692ccca9c88bafe5dcb6f8a62a4c6bc55cd9ba82bb2cd95f6c.svg"
+      />
     </div>
   );
 };
